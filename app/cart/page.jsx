1c0cd8d001746c30114afecb38d9a1040d242118
khalek-repliq <abdul.khalek@repliq.dev>
2024-10-28 16:39:26 +0600

    added a commit

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -4,6 +4,7 @@ const Cart = () => {
   return (
     <div className="bg-gray-50 min-h-screen flex items-center">
       <div className="container mx-auto">
+        {/* Cart page */}
         <h1 className="text-4xl">This is the cart page</h1>
       </div>
     </div>
